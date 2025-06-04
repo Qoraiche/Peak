15f0c8bdead46f29f351dc1c22b58829ada57790
Yassine Qoraiche <15586492+Qoraiche@users.noreply.github.com>
2025-06-04 22:12:09 +0100

    free migration in the house

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,8 +4,6 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -15,19 +13,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(RolesAndPermissionsSeeder::class); // Ensure the roles are created first
         $this->call(UsersTableSeeder::class);
-        $this->call(MenuSeeder::class);
-        $this->call(PageSeeder::class);
-        $this->call(RoadmapSeeder::class);
-        $this->call(SubscriptionPlanSeeder::class);
-        $this->call(PostSeeder::class);
-        $this->call(DocSeeder::class);
-        $this->call(ChangelogSeeder::class);
-        $this->call(RoadmapSeeder::class);
-        $this->call(FaqSeeder::class);
-        $this->call(FeatureSeeder::class);
-        $this->call(SubscriptionPlanSeeder::class);
-        $this->call(TicketSeeder::class);
         $this->call(LanguageSeeder::class);
-        $this->call(TestimonialSeeder::class);
     }
 }
