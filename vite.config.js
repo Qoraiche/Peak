3f0479fbb96e8c57b28ed659613dd4d74ffe2051
Yassine Qoraiche <15586492+Qoraiche@users.noreply.github.com>
2025-06-04 12:43:28 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,14 +6,11 @@ import {defineConfig} from "vite";
 export default defineConfig({
     plugins: [
         laravel({
-
             input: [
-                'peak/resources/js/admin.js',
                 'resources/js/Themes/Breeze/theme.js',
-                'resources/js/Themes/PeekyPok/theme.js',
+                'peak/resources/js/admin.js',
             ],
-
-            ssr: "resources/js/ssr.js",
+            ssr: 'resources/js/ssr.js',
             refresh: true,
         }),
         vue({
@@ -26,17 +23,45 @@ export default defineConfig({
         }),
     ],
 
-    ssr: {
-        noExternal: ['@inertiajs/server'],
-    },
-
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "resources/js"),
             "@peak": path.resolve(__dirname, "peak/resources/js"),
-            "@Breeze": path.resolve(__dirname, "resources/js/Themes/Breeze"), // theme alias
-            "@PeekyPok": path.resolve(__dirname, "resources/js/Themes/PeekyPok"), // theme alias
+            "@Breeze": path.resolve(__dirname, "resources/js/Themes/Breeze"),
             "ziggy": path.resolve(__dirname, "vendor/tightenco/ziggy"),
+        },
+    },
+
+    build: {
+        cssCodeSplit: true, // keep CSS code splitting (optional)
+        target: 'es2018', // support modern browsers only; improve performance
+        sourcemap: false, // disable for smaller build size (unless debugging)
+        minify: 'esbuild', // faster than terser
+        brotliSize: false, // disables brotli size calc for faster builds
+        rollupOptions: {
+            output: {
+                // Let Vite handle chunking except vendor separation
+                manualChunks(id) {
+                    if (id.includes('node_modules')) {
+                        if (id.includes('vue')) {
+                            return 'vendor-vue';
+                        }
+                        if (id.includes('inertia')) {
+                            return 'vendor-inertia';
+                        }
+                        if (id.includes('lodash')) {
+                            return 'vendor-lodash';
+                        }
+                        if (id.includes('axios')) {
+                            return 'vendor-axios';
+                        }
+                        return 'vendor-others'; // fallback for all other node_modules
+                    }
+                },
+                chunkFileNames: 'assets/[name]-[hash].js',
+                entryFileNames: 'assets/[name]-[hash].js',
+                assetFileNames: 'assets/[name]-[hash].[ext]',
+            },
         },
     },
 });
